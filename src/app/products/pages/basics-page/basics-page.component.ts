cea180c974d0daa8d3b90c422e2fc7169cfc8d13
DanielHenryL <lagunasdaniel001@gmail.com>
2023-12-15 23:35:34 -0500

    primeFlex, upper,lower,title case y date pipe

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './basics-page.component.css'
 })
 export class BasicsPageComponent {
-
+  public nameLower:string = 'daniel';
+  public nameUpper:string = 'DANIEL';
+  public fullName:string = 'DaNiEl LaGuNaS';
+  public customDate:Date = new Date();
 }

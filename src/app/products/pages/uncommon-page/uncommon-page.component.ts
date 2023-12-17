3d94f8ec9e12d1b4f568b44d01ecc41bd253d57f
DanielHenryL <lagunasdaniel001@gmail.com>
2023-12-17 12:59:23 -0500

    i18nSelect Pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './uncommon-page.component.css'
 })
 export class UncommonPageComponent {
+  public name :string = 'Daniel';
+  public gender: 'male' | 'female' = 'male';
 
+  public invitationMap = {
+    male:'invitarlo',
+    female:'invitarla'
+  }
+  changeClient(){
+    this.name = this.name === 'Daniel' ? 'Daniela' : 'Daniel';
+    this.gender = this.gender === 'male' ? 'female':'male';
+  }
 }

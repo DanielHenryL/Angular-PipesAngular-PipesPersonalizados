31f5d86db3eecee5c4db630ae55598f2388b9e0c
DanielHenryL <lagunasdaniel001@gmail.com>
2023-12-17 13:24:46 -0500

    i18nPlural Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './uncommon-page.component.css'
 })
 export class UncommonPageComponent {
+
+  // i18nSelect
   public name :string = 'Daniel';
   public gender: 'male' | 'female' = 'male';
 
@@ -16,5 +18,17 @@ export class UncommonPageComponent {
   changeClient(){
     this.name = this.name === 'Daniel' ? 'Daniela' : 'Daniel';
     this.gender = this.gender === 'male' ? 'female':'male';
+  }
+
+  // i18nPlural
+
+  public clients:string[] = ['Yosi','Thiago','Daniel','Freddy','Lourdes']
+  public clientsMap = {
+    '=0':', no tenemos ningun cliente',
+    '=1':' tenemos un cliente',
+    'other':' tenemos # clientes',
+  }
+  deleteClient(){
+    this.clients.shift()
   }
 }

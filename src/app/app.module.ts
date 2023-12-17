ff846fd2d4ca5d4eec3b22221702894b510413b5
DanielHenryL <lagunasdaniel001@gmail.com>
2023-12-16 20:51:27 -0500

    Date pipes y cambio de idioma global y especifico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+
+import localeEsPe from '@angular/common/locales/es-PE';
+import localeFr from '@angular/common/locales/fr';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData( localeEsPe );
+registerLocaleData( localeFr );
 
 @NgModule({
   declarations: [
@@ -17,7 +24,9 @@ import { SharedModule } from './shared/shared.module';
 
     SharedModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue:'es-PE'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

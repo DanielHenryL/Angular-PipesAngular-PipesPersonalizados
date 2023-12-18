3d736efb946aed02f0f47522d898ec31307bfe15
DanielHenryL <lagunasdaniel001@gmail.com>
2023-12-17 20:31:15 -0500

    async Pipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable, interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
@@ -38,4 +39,15 @@ export class UncommonPageComponent {
     age:26,
     address:'Peru, lima'
   }
+  //async
+  public myObsevableTimer:Observable<number> = interval(2000).pipe(
+    tap( value => console.log('tap:',value ))
+  )
+  public promiseValue:Promise<string> = new Promise( (resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de la promesa');
+      console.log( 'Tenemos data de la promesa' );
+      this.person.name = 'Henry'
+    }, 4000);
+  })
 }

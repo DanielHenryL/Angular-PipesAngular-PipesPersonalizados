2377baf4830585c94b09b4bfc013ce2c9dd47d9d
DanielHenryL <lagunasdaniel001@gmail.com>
2023-12-18 22:40:33 -0500

    Valores y argumentos personalizados

--- src/app/products/pages/order-page/order-page.component.ts
+++ src/app/products/pages/order-page/order-page.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
   styles: ``
 })
-export class OrderPageComponent {
+export class OrderPageComponent implements OnInit{
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+      this.items = [
+          {
+              label: 'Update',
+              icon: 'pi pi-refresh'
+          },
+          {
+              label: 'Delete',
+              icon: 'pi pi-times'
+          }
+      ];
+  }
 }
